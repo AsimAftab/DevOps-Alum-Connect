5e21d3c7c3881aa3d2afc7f52802f607387ab33a
Asim Aftab <asim.dev.cs@gmail.com>
2025-06-02 13:55:55 +0530

    Added CI/CD with Docker,TerraForm, and Azure

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "devops-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+  kind = "Linux"
+  reserved = true
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "devops-alum-connect"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|asimaftab47/devops-alum-connect:latest"
+    always_on        = true
+  }
+}
